4c4f06d3f3fa4b244aaf8a71207abf85e057c225
demohi <mds@xue.bi>
2015-11-14 17:37:54 +0800

    add postcss & autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var scss = require('gulp-scss');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var processors = [
+    autoprefixer({browsers: ['last 2 version']})
+];
 
 gulp.task('compile', function(){
   gulp.src('./scss/ie.scss')
       .pipe(scss())
+      .pipe(postcss(processors))
       .pipe(gulp.dest("css/"))
 });
 
